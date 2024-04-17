10bc88d569e544e6d08a53ad7b1f8c83eb1a5644
sirusuvada <133570641+sirusuvada@users.noreply.github.com>
2024-04-18 03:07:36 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -6,3 +6,6 @@ import streamlit as st
 st.success('Mounu Love you ra')                                  # Success
 
 hobbies = st.text_input('Write about your hobbies here:')
+if hobbies:
+    with open('hobbies.txt', 'a') as file:
+        file.write(hobbies + '\n')
