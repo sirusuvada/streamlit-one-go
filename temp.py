c43201a3b2e2055c8ab29ad3a206c5eaf0da8d96
sirusuvada <133570641+sirusuvada@users.noreply.github.com>
2024-04-18 03:14:12 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,5 +7,5 @@ st.success('Mounu Love you ra')                                  # Success
 
 hobbies = st.text_input('Write about your hobbies here:')
 if hobbies:
-    with open('hobbies.txt', 'a') as file:
+    with open('untitled.txt', 'a') as file:
         file.write(hobbies + '\n')
